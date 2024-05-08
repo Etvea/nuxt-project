7da8e1f705470538d2613cae7099ca0f47306c13
Etvea <senturilarra@gmail.com>
2024-05-08 21:19:35 +0200

    First commit - uploading all files to repository.

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor </p>
+        <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+   h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+   }
+   p {
+    margin: 20px, 0;
+   }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Products details for {{ id }}</p>
+        <p>Lorem ipsum dolor</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
