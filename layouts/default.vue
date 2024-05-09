af7c04d8b31959380de2aab8f3592b2e986cccf4
Etvea <senturilarra@gmail.com>
2024-05-09 09:21:42 +0200

    "Added tailwindcss"

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,15 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
-                <NuxtLink to="/">Nuxt Project</NuxtLink>
-                <ul>
+                <NuxtLink to="/" class="font-bold">Nuxt Project</NuxtLink>
+                <ul class="flex gap-4">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                    <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
                 </ul>
             </nav>
         </header>
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
     </div>
