2c1fc719b976b0f3ec8c0106669936166226966c
Etvea <senturilarra@gmail.com>
2024-05-09 09:38:42 +0200

    "Added fetching data from fakestoreapi.com"

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        <p>Products details for {{ id }}</p>
-        <p>Lorem ipsum dolor</p>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.id }}</p>
     </div>
 </template>
 
 <script setup>
     const { id } = useRoute().params
+    const uri = 'https://fakestoreapi.com/products/' + id
+
+    //fetch the product
+    const { data: product } = await useFetch(uri, {key: id})
+
+    definePageMeta({
+        layout: 'products'
+    })
 </script>
 
 <style scoped>
